e681bc6c690fefab5cb10d06781ad7708ad15944
chdacamp <43750486+chdacamp@users.noreply.github.com>
2019-02-26 19:57:00 -0800

    fix weird dash character in lon on dataset

--- static/data/create_json_from_csv.py
+++ static/data/create_json_from_csv.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("./final w lat lon.csv",encoding = "ISO-8859-1")
 
-df.to_json("./final w lat long.json")
+df = pd.to_numeric(df["Lon"])
+
+df.to_json("./final w lat long.json",orient = "records")
