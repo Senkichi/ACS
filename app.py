1afe6f517aaeb8041a7a02aaa53a2c5ad0e53f5c
HJhutty <hsjsingh@ucdavis.edu>
2019-02-27 16:19:45 -0800

    updated database configuration & added data route

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import pymongo
+from flask import Flask, render_template, jsonify
+from pymongo import Pymongo
+from random import sample
 import time
 
 app = Flask(__name__)
 
-# Configure MongoDB Database
-conn = 'mongodb://root:ucbmongodb@35.184.4.63:27017'
-client = pymongo.MongoClient(conn)
-db = client.somedb
-db.someinfo.insert_one(temp_doc)
+# # Configure MongoDB Database
+app.config['MONGO_DBNAME'] = 'ACS'
+app.config['MONGO_URI'] = 'mongodb://root:ucbmongodb@35.184.4.63:27017/ACS'
+
+mongo = Pymongo(app)
+
+# conn = 'mongodb://root:ucbmongodb@35.184.4.63:27017'
+# client = pymongo.MongoClient(conn)
+# db = client.somedb
+# db.someinfo.insert_one(temp_doc)
 
 
 
@@ -18,3 +23,15 @@ db.someinfo.insert_one(temp_doc)
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("data")
+def data():
+    acs = mongo.db.acs
+
+    result = acs.find_all({'County'})
+
+    return jsonify({'results': result['County']})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
