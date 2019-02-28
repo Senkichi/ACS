b65ceaa952ac9b4399152fe371f035d188e72287
chdacamp <43750486+chdacamp@users.noreply.github.com>
2019-02-27 17:04:41 -0800

    working dropdowns for census map

--- static/templates/js/app.js
+++ static/templates/js/app.js
@@ -1,25 +1,37 @@
 
 var url = "./js/final_w_lat_lon.json"
 
-var myMap = L.map("map", {
+var map = L.map("map", {
     center: [
       36.7783, -119.4179
     ],
     zoom: 6,
   });
 
+  
+  
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets-basic",
   accessToken: API_KEY
-}).addTo(myMap);
+}).addTo(map);
 
-// Perform a GET request to the query URL
+var group1 = L.featureGroup();
+var	yearSelect = d3.select("#year").node().value;
+var	typeSelect = d3.select("#dataType").node().value;
+
+//initial draw
 d3.json(url, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data);
-});
+
+	createFeatures(data);
+})
+
+
+
+// Perform a GET request to the query URL
+
 
 
 function createFeatures(sourceData) {
@@ -28,36 +40,52 @@ function createFeatures(sourceData) {
 
 
 for (var i = 0; i < sourceData.length; i++) {
-
-/*   // Conditionals for magnitude
-  var color = "";
-  if (sourceData[i].properties.mag > 5) {
-    color = "red";
-  }
-  else if (sourceData[i].properties.mag > 4) {
-    color = "orange";
-  }
-  else if (sourceData[i].properties.mag > 3) {
-    color = "yellow";
-  }
-  else {
-    color = "green";
-  }
- */
+ 
+ 
 var coords = [sourceData[i].Lat,sourceData[i].Lon]
-
-var selectData = sourceData[i]["2016RENT"]
+var selectConcat = String(yearSelect.concat(typeSelect))
+var selectData = sourceData[i][selectConcat]
 
   // Add circles to map
 L.circle(coords,{
-    radius: selectData*5,
+    radius: selectData,
     fillColor: "blue",
     color: "black",
     weight: 1,
     opacity: 1,
     fillOpacity: 0.8
-}).addTo(myMap).bindPopup("<h3>" + sourceData[i].NAME);
+}).addTo(group1).bindPopup("<h5>" + sourceData[i].NAME);
+
+map.addLayer(group1);
+	  }
 
 
+};
 
-}};
+d3.json(url, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+
+ d3.select("#year").on("change",function(){
+ 
+	yearSelect = d3.select("#year").node().value;
+	console.log(yearSelect);
+	group1.clearLayers()
+	createFeatures(data);
+
+	})
+})
+
+d3.json(url, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+
+
+ d3.select("#dataType").on("change",function(){
+			
+	typeSelect = d3.select("#dataType").node().value;;
+	console.log(typeSelect)
+	group1.clearLayers()
+	createFeatures(data);
+
+	})
+})
+
